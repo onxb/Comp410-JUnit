848e2ae360dd0c7e98bd3b4f97fdaff7cbadedb2
Chris <revisedboomerguy@gmail.com>
2017-02-14 10:31:31 -0500

    Format and allow for both remove cases
    Formatting: Put everything in each method inside the try catch statements.
    
    Remove Cases: Check for either left subtree findMax or right subtree FindMin cases in testRemoveTwoChildren and testHeightRemoveTwoChildren

--- A2/BSTTest.java
+++ A2/BSTTest.java
@@ -8,19 +8,20 @@ public class BSTTest {
 
 	@Test
 	public void testFindMin() {
-		BST tree = new BST();
-		tree.insert("gg");
-		tree.insert("dd");
-		tree.insert("ss");
-		tree.insert("ee");
-		tree.insert("cc");
-		tree.insert("zz");
-		tree.insert("gg");
-		tree.insert("bb");
-		tree.insert("ff");
-		tree.insert("aa");
-
 		try {
+			BST tree = new BST();
+
+			tree.insert("gg");
+			tree.insert("dd");
+			tree.insert("ss");
+			tree.insert("ee");
+			tree.insert("cc");
+			tree.insert("zz");
+			tree.insert("gg");
+			tree.insert("bb");
+			tree.insert("ff");
+			tree.insert("aa");
+
 			assertEquals("findMin returns wrong node", "aa", tree.findMin());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -29,19 +30,20 @@ public class BSTTest {
 
 	@Test
 	public void testFindMax() {
-		BST tree = new BST();
-		tree.insert("gg");
-		tree.insert("dd");
-		tree.insert("ss");
-		tree.insert("ee");
-		tree.insert("cc");
-		tree.insert("zz");
-		tree.insert("gg");
-		tree.insert("bb");
-		tree.insert("ff");
-		tree.insert("aa");
-
 		try {
+			BST tree = new BST();
+
+			tree.insert("gg");
+			tree.insert("dd");
+			tree.insert("ss");
+			tree.insert("ee");
+			tree.insert("cc");
+			tree.insert("zz");
+			tree.insert("gg");
+			tree.insert("bb");
+			tree.insert("ff");
+			tree.insert("aa");
+
 			assertEquals("findMax returns wrong node", "zz", tree.findMax());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -50,19 +52,20 @@ public class BSTTest {
 
 	@Test
 	public void testContainsTrue() {
-		BST tree = new BST();
-		tree.insert("gg");
-		tree.insert("dd");
-		tree.insert("ss");
-		tree.insert("ee");
-		tree.insert("cc");
-		tree.insert("zz");
-		tree.insert("gg");
-		tree.insert("bb");
-		tree.insert("ff");
-		tree.insert("aa");
-
 		try {
+			BST tree = new BST();
+
+			tree.insert("gg");
+			tree.insert("dd");
+			tree.insert("ss");
+			tree.insert("ee");
+			tree.insert("cc");
+			tree.insert("zz");
+			tree.insert("gg");
+			tree.insert("bb");
+			tree.insert("ff");
+			tree.insert("aa");
+
 			assertTrue("contains returns false", tree.contains("cc"));
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -71,19 +74,20 @@ public class BSTTest {
 
 	@Test
 	public void testContainsFalse() {
-		BST tree = new BST();
-		tree.insert("gg");
-		tree.insert("dd");
-		tree.insert("ss");
-		tree.insert("ee");
-		tree.insert("cc");
-		tree.insert("zz");
-		tree.insert("gg");
-		tree.insert("bb");
-		tree.insert("ff");
-		tree.insert("aa");
-
 		try {
+			BST tree = new BST();
+
+			tree.insert("gg");
+			tree.insert("dd");
+			tree.insert("ss");
+			tree.insert("ee");
+			tree.insert("cc");
+			tree.insert("zz");
+			tree.insert("gg");
+			tree.insert("bb");
+			tree.insert("ff");
+			tree.insert("aa");
+
 			assertFalse("contains returns true", tree.contains("ad"));
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -92,9 +96,9 @@ public class BSTTest {
 
 	@Test
 	public void testEmptyTrue() {
-		BST tree = new BST();
-
 		try {
+			BST tree = new BST();
+
 			assertTrue("empty returns false", tree.empty());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -103,11 +107,11 @@ public class BSTTest {
 
 	@Test
 	public void testEmptyFalse() {
-		BST tree = new BST();
-
-		tree.insert("h");
-
 		try {
+			BST tree = new BST();
+
+			tree.insert("h");
+
 			assertFalse("empty returns true", tree.empty());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -116,17 +120,18 @@ public class BSTTest {
 
 	@Test
 	public void testHeightInsertOnly() {
-		BST tree = new BST();
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
-
 		try {
+			BST tree = new BST();
+
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
+
 			assertEquals("height returned wrong height", 5, tree.height());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -135,17 +140,19 @@ public class BSTTest {
 
 	@Test
 	public void testHeightRemoveLeaf() {
-		BST tree = new BST();
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
-		tree.remove("f");
 		try {
+			BST tree = new BST();
+
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
+			tree.remove("f");
+
 			assertEquals("height returned wrong height", 4, tree.height());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -154,17 +161,19 @@ public class BSTTest {
 
 	@Test
 	public void testHeightRemoveOneChild() {
-		BST tree = new BST();
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
-		tree.remove("g");
 		try {
+			BST tree = new BST();
+
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
+			tree.remove("g");
+
 			assertEquals("height returned wrong height", 4, tree.height());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -173,18 +182,30 @@ public class BSTTest {
 
 	@Test
 	public void testHeightRemoveTwoChildren() {
-		BST tree = new BST();
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
-		tree.remove("d");
 		try {
-			assertEquals("height returned wrong height", 4, tree.height());
+			BST tree = new BST();
+
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
+			tree.remove("d");
+
+			if (tree.root.right.data.equals("c")) {
+				assertEquals("height returned wrong height", 5, tree.height());
+			} else if (tree.root.right.data.equals("e")) {
+				assertEquals("height returned wrong height", 4, tree.height());
+			} else {
+				throw new Exception("Remove two children didn't work properly."
+						+ "\nExpected c if using left subtree findMax"
+						+ "\nor expected e if using right subtree findMin"
+						+ " but received: "
+						+ tree.root.right.data);
+			}
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
 		}
@@ -192,16 +213,18 @@ public class BSTTest {
 
 	@Test
 	public void testSizeInsertOnly() {
-		BST tree = new BST();
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
 		try {
+			BST tree = new BST();
+
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
+
 			assertEquals("size returned wrong size", 8, tree.size());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -210,17 +233,20 @@ public class BSTTest {
 
 	@Test
 	public void testSizeRemove() {
-		BST tree = new BST();
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
-		tree.remove("s");
+
 		try {
+			BST tree = new BST();
+
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
+			tree.remove("s");
+
 			assertEquals("size returned wrong size", 7, tree.size());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -229,48 +255,24 @@ public class BSTTest {
 
 	@Test
 	public void testInsert() {
-		BST tree = new BST();
-
 		try {
+			BST tree = new BST();
+
 			assertTrue("insert returned false", tree.insert("a"));
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
-
-		try {
 			assertTrue("insert returned false", tree.insert("d"));
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
-
-		try {
 			assertTrue("insert returned false", tree.insert("c"));
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
 
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
 
-		try {
 			assertEquals("inserted wrong node into spot", "s", tree.root.right.right.getData());
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
-
-		try {
 			assertEquals("inserted wrong node into spot", "g", tree.root.right.right.left.right.getData());
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
-
-		try {
 			assertFalse("insert returned true", tree.insert("a"));
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -279,36 +281,21 @@ public class BSTTest {
 
 	@Test
 	public void testRemoveNoChildren() {
-		BST tree = new BST();
-
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
-
 		try {
+			BST tree = new BST();
+
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
+
 			assertTrue("remove no children case returned false", tree.remove("f"));
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
-
-		try {
 			assertEquals("remove no children case didn't remove", null, tree.root.right.right.left.right.left);
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
-
-		try {
 			assertTrue("remove 1 child case returned false", tree.remove("e"));
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
-
-		try {
 			assertEquals("remove 1 child case didn't remove properly", "g", tree.root.right.right.left.getData());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -317,24 +304,19 @@ public class BSTTest {
 
 	@Test
 	public void testRemoveOneChild() {
-		BST tree = new BST();
-
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
-
 		try {
+			BST tree = new BST();
+
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
+
 			assertTrue("remove 1 child case returned false", tree.remove("e"));
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
-
-		try {
 			assertEquals("remove 1 child case didn't remove properly", "g", tree.root.right.right.left.getData());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
@@ -343,25 +325,30 @@ public class BSTTest {
 
 	@Test
 	public void testRemoveTwoChildren() {
-		BST tree = new BST();
-
-		tree.insert("a");
-		tree.insert("d");
-		tree.insert("s");
-		tree.insert("e");
-		tree.insert("c");
-		tree.insert("g");
-		tree.insert("b");
-		tree.insert("f");
-
 		try {
-			assertTrue("remove 2 children case returned false", tree.remove("d"));
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
+			BST tree = new BST();
 
-		try {
-			assertFalse("remove 2 children didn't remove properly", tree.contains("d"));
+			tree.insert("a");
+			tree.insert("d");
+			tree.insert("s");
+			tree.insert("e");
+			tree.insert("c");
+			tree.insert("g");
+			tree.insert("b");
+			tree.insert("f");
+			tree.insert("w");
+			tree.insert("z");
+			tree.insert("y");
+			tree.insert("u");
+
+			assertTrue("remove 2 children case returned false", tree.remove("s"));
+			assertFalse("remove 2 children didn't remove properly", tree.contains("s"));
+			assertTrue("remove 2 children didn't replace node properly"
+					+ "\n expected g if using left subtree findMax"
+					+ "\n or expected u if using right subtree findMin"
+					+ "\n but received: " + tree.root.right.right,
+					tree.root.right.right.data.equals("u")
+					|| tree.root.right.right.data.equals("g"));
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
 		}
