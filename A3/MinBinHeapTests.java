5ec4d84bdab381de8ee88a41c06f072f65d16cd0
David Ben Knoble <ben.knoble@gmail.com>
2017-03-01 23:07:18 -0500

    Eclipse format ... eh

--- A3/MinBinHeapTests.java
+++ A3/MinBinHeapTests.java
@@ -19,14 +19,14 @@ public class MinBinHeapTests {
     public void testSizeFilledInserts() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            heap.insert(new EntryPair("d",10));
-            heap.insert(new EntryPair("s",4));
-            heap.insert(new EntryPair("g",7));
-            heap.insert(new EntryPair("b",5));
-            heap.insert(new EntryPair("z",1));
-            heap.insert(new EntryPair("c",0));
-            heap.insert(new EntryPair("e",11));
-            heap.insert(new EntryPair("j",3));
+            heap.insert(new EntryPair("d", 10));
+            heap.insert(new EntryPair("s", 4));
+            heap.insert(new EntryPair("g", 7));
+            heap.insert(new EntryPair("b", 5));
+            heap.insert(new EntryPair("z", 1));
+            heap.insert(new EntryPair("c", 0));
+            heap.insert(new EntryPair("e", 11));
+            heap.insert(new EntryPair("j", 3));
             assertEquals("size returned wrong size", 8, heap.size());
         } catch (Exception e) {
             fail("Exception thrown: " + e.getMessage());
@@ -37,15 +37,15 @@ public class MinBinHeapTests {
     public void testSizeFilledBuild() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            EntryPair[] collection= new EntryPair[8];
-            collection[0]=new EntryPair("d",10);
-            collection[1]=new EntryPair("s",4);
-            collection[2]=new EntryPair("g",7);
-            collection[3]=new EntryPair("b",5);
-            collection[4]=new EntryPair("z",1);
-            collection[5]=new EntryPair("c",0);
-            collection[6]=new EntryPair("e",11);
-            collection[7]=new EntryPair("j",3);
+            EntryPair[] collection = new EntryPair[8];
+            collection[0] = new EntryPair("d", 10);
+            collection[1] = new EntryPair("s", 4);
+            collection[2] = new EntryPair("g", 7);
+            collection[3] = new EntryPair("b", 5);
+            collection[4] = new EntryPair("z", 1);
+            collection[5] = new EntryPair("c", 0);
+            collection[6] = new EntryPair("e", 11);
+            collection[7] = new EntryPair("j", 3);
             heap.build(collection);
             assertEquals("size returned wrong size", 8, heap.size());
         } catch (Exception e) {
@@ -67,15 +67,15 @@ public class MinBinHeapTests {
     public void testGetMinFilledInsert() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            EntryPair minPair = new EntryPair("c",0);
-            heap.insert(new EntryPair("d",10));
-            heap.insert(new EntryPair("s",4));
-            heap.insert(new EntryPair("g",7));
-            heap.insert(new EntryPair("b",5));
-            heap.insert(new EntryPair("z",1));
+            EntryPair minPair = new EntryPair("c", 0);
+            heap.insert(new EntryPair("d", 10));
+            heap.insert(new EntryPair("s", 4));
+            heap.insert(new EntryPair("g", 7));
+            heap.insert(new EntryPair("b", 5));
+            heap.insert(new EntryPair("z", 1));
             heap.insert(minPair);
-            heap.insert(new EntryPair("e",11));
-            heap.insert(new EntryPair("j",3));
+            heap.insert(new EntryPair("e", 11));
+            heap.insert(new EntryPair("j", 3));
             assertEquals("getMin returned wrong pair", minPair, heap.getMin());
         } catch (Exception e) {
             fail("Exception thrown: " + e.getMessage());
@@ -86,16 +86,16 @@ public class MinBinHeapTests {
     public void testGetMinFilledBuild() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            EntryPair minPair = new EntryPair("c",0);
-            EntryPair[] collection= new EntryPair[8];
-            collection[0]=new EntryPair("d",10);
-            collection[1]=new EntryPair("s",4);
-            collection[2]=new EntryPair("g",7);
-            collection[3]=new EntryPair("b",5);
-            collection[4]=new EntryPair("z",1);
-            collection[5]=minPair;
-            collection[6]=new EntryPair("e",11);
-            collection[7]=new EntryPair("j",3);
+            EntryPair minPair = new EntryPair("c", 0);
+            EntryPair[] collection = new EntryPair[8];
+            collection[0] = new EntryPair("d", 10);
+            collection[1] = new EntryPair("s", 4);
+            collection[2] = new EntryPair("g", 7);
+            collection[3] = new EntryPair("b", 5);
+            collection[4] = new EntryPair("z", 1);
+            collection[5] = minPair;
+            collection[6] = new EntryPair("e", 11);
+            collection[7] = new EntryPair("j", 3);
             heap.build(collection);
             assertEquals("getMin returned wrong pair", minPair, heap.getMin());
         } catch (Exception e) {
@@ -107,7 +107,7 @@ public class MinBinHeapTests {
     public void testInsertA() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            EntryPair pair = new EntryPair("c",5);
+            EntryPair pair = new EntryPair("c", 5);
             heap.insert(pair);
             assertEquals("inserted wrong pair", pair, heap.getHeap()[1]);
         } catch (Exception e) {
@@ -119,25 +119,24 @@ public class MinBinHeapTests {
     public void testInsertB() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            heap.insert(new EntryPair("d",15));
-            heap.insert(new EntryPair("s",30));
-            heap.insert(new EntryPair("g",4));
-            heap.insert(new EntryPair("b",38));
-            heap.insert(new EntryPair("z",12));
-            heap.insert(new EntryPair("c",18));
-            heap.insert(new EntryPair("e",6));
-            heap.insert(new EntryPair("j",0));
-            heap.insert(new EntryPair("a",24));
-            heap.insert(new EntryPair("o",14));
+            heap.insert(new EntryPair("d", 15));
+            heap.insert(new EntryPair("s", 30));
+            heap.insert(new EntryPair("g", 4));
+            heap.insert(new EntryPair("b", 38));
+            heap.insert(new EntryPair("z", 12));
+            heap.insert(new EntryPair("c", 18));
+            heap.insert(new EntryPair("e", 6));
+            heap.insert(new EntryPair("j", 0));
+            heap.insert(new EntryPair("a", 24));
+            heap.insert(new EntryPair("o", 14));
 
-            int[] collection= {0,4,6,12,14,18,15,38,24,30};
+            int[] collection = { 0, 4, 6, 12, 14, 18, 15, 38, 24, 30 };
             int[] heapPriorities = new int[10];
             EntryPair[] heapArray = heap.getHeap();
-            for (int i=0;i<heapPriorities.length;i++) {
-                heapPriorities[i] = heapArray[i+1].getPriority();
+            for (int i = 0; i < heapPriorities.length; i++) {
+                heapPriorities[i] = heapArray[i + 1].getPriority();
             }
-            assertArrayEquals("insert created different heap than expected",
-                    collection, heapPriorities);
+            assertArrayEquals("insert created different heap than expected", collection, heapPriorities);
         } catch (Exception e) {
             fail("Exception thrown: " + e.getMessage());
         }
@@ -148,8 +147,7 @@ public class MinBinHeapTests {
         try {
             MinBinHeap heap = new MinBinHeap();
             heap.delMin();
-            assertTrue("delMin of empty heap gives min when it shouldn't",
-                    heap.getMin()==null);
+            assertTrue("delMin of empty heap gives min when it shouldn't", heap.getMin() == null);
         } catch (Exception e) {
             fail("Exception thrown: " + e.getMessage());
         }
@@ -159,26 +157,25 @@ public class MinBinHeapTests {
     public void testDelMinB() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            heap.insert(new EntryPair("d",15));
-            heap.insert(new EntryPair("s",30));
-            heap.insert(new EntryPair("g",4));
-            heap.insert(new EntryPair("b",38));
-            heap.insert(new EntryPair("z",12));
-            heap.insert(new EntryPair("c",18));
-            heap.insert(new EntryPair("e",6));
-            heap.insert(new EntryPair("j",0));
-            heap.insert(new EntryPair("a",24));
-            heap.insert(new EntryPair("o",14));
+            heap.insert(new EntryPair("d", 15));
+            heap.insert(new EntryPair("s", 30));
+            heap.insert(new EntryPair("g", 4));
+            heap.insert(new EntryPair("b", 38));
+            heap.insert(new EntryPair("z", 12));
+            heap.insert(new EntryPair("c", 18));
+            heap.insert(new EntryPair("e", 6));
+            heap.insert(new EntryPair("j", 0));
+            heap.insert(new EntryPair("a", 24));
+            heap.insert(new EntryPair("o", 14));
             heap.delMin();
 
-            int[] collection= {4,12,6,24,14,18,15,38,30};
+            int[] collection = { 4, 12, 6, 24, 14, 18, 15, 38, 30 };
             int[] heapPriorities = new int[9];
             EntryPair[] heapArray = heap.getHeap();
-            for (int i=0;i<heapPriorities.length;i++) {
-                heapPriorities[i] = heapArray[i+1].getPriority();
+            for (int i = 0; i < heapPriorities.length; i++) {
+                heapPriorities[i] = heapArray[i + 1].getPriority();
             }
-            assertArrayEquals("delMin created different heap than expected"
-                    , collection, heapPriorities);
+            assertArrayEquals("delMin created different heap than expected", collection, heapPriorities);
         } catch (Exception e) {
             fail("Exception thrown: " + e.getMessage());
         }
@@ -188,28 +185,27 @@ public class MinBinHeapTests {
     public void testDelMinC() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            EntryPair[] collectionToBuild= new EntryPair[10];
-            collectionToBuild[0]=new EntryPair("d",15);
-            collectionToBuild[1]=new EntryPair("s",30);
-            collectionToBuild[2]=new EntryPair("g",4);
-            collectionToBuild[3]=new EntryPair("b",38);
-            collectionToBuild[4]=new EntryPair("z",12);
-            collectionToBuild[5]=new EntryPair("c",18);
-            collectionToBuild[6]=new EntryPair("e",6);
-            collectionToBuild[7]=new EntryPair("j",0);
-            collectionToBuild[8]=new EntryPair("a",24);
-            collectionToBuild[9]=new EntryPair("o",14);
+            EntryPair[] collectionToBuild = new EntryPair[10];
+            collectionToBuild[0] = new EntryPair("d", 15);
+            collectionToBuild[1] = new EntryPair("s", 30);
+            collectionToBuild[2] = new EntryPair("g", 4);
+            collectionToBuild[3] = new EntryPair("b", 38);
+            collectionToBuild[4] = new EntryPair("z", 12);
+            collectionToBuild[5] = new EntryPair("c", 18);
+            collectionToBuild[6] = new EntryPair("e", 6);
+            collectionToBuild[7] = new EntryPair("j", 0);
+            collectionToBuild[8] = new EntryPair("a", 24);
+            collectionToBuild[9] = new EntryPair("o", 14);
             heap.build(collectionToBuild);
             heap.delMin();
 
-            int[] collection= {4,12,6,24,14,18,15,38,30};
+            int[] collection = { 4, 12, 6, 24, 14, 18, 15, 38, 30 };
             int[] heapPriorities = new int[9];
             EntryPair[] heapArray = heap.getHeap();
-            for (int i=0;i<heapPriorities.length;i++) {
-                heapPriorities[i] = heapArray[i+1].getPriority();
+            for (int i = 0; i < heapPriorities.length; i++) {
+                heapPriorities[i] = heapArray[i + 1].getPriority();
             }
-            assertArrayEquals("delMin created different heap than expected"
-                    , collection, heapPriorities);
+            assertArrayEquals("delMin created different heap than expected", collection, heapPriorities);
         } catch (Exception e) {
             fail("Exception thrown: " + e.getMessage());
         }
@@ -219,10 +215,9 @@ public class MinBinHeapTests {
     public void testBuildA() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            EntryPair[] collectionToBuild= new EntryPair[0];
+            EntryPair[] collectionToBuild = new EntryPair[0];
             heap.build(collectionToBuild);
-            assertTrue("build with empty collection doesn't keep an empty heap",
-                    heap.getMin()==null);
+            assertTrue("build with empty collection doesn't keep an empty heap", heap.getMin() == null);
         } catch (Exception e) {
             fail("Exception thrown: " + e.getMessage());
         }
@@ -232,27 +227,26 @@ public class MinBinHeapTests {
     public void testBuildB() {
         try {
             MinBinHeap heap = new MinBinHeap();
-            EntryPair[] collectionToBuild= new EntryPair[10];
-            collectionToBuild[0]=new EntryPair("d",15);
-            collectionToBuild[1]=new EntryPair("s",30);
-            collectionToBuild[2]=new EntryPair("g",4);
-            collectionToBuild[3]=new EntryPair("b",38);
-            collectionToBuild[4]=new EntryPair("z",12);
-            collectionToBuild[5]=new EntryPair("c",18);
-            collectionToBuild[6]=new EntryPair("e",6);
-            collectionToBuild[7]=new EntryPair("j",0);
-            collectionToBuild[8]=new EntryPair("a",24);
-            collectionToBuild[9]=new EntryPair("o",14);
+            EntryPair[] collectionToBuild = new EntryPair[10];
+            collectionToBuild[0] = new EntryPair("d", 15);
+            collectionToBuild[1] = new EntryPair("s", 30);
+            collectionToBuild[2] = new EntryPair("g", 4);
+            collectionToBuild[3] = new EntryPair("b", 38);
+            collectionToBuild[4] = new EntryPair("z", 12);
+            collectionToBuild[5] = new EntryPair("c", 18);
+            collectionToBuild[6] = new EntryPair("e", 6);
+            collectionToBuild[7] = new EntryPair("j", 0);
+            collectionToBuild[8] = new EntryPair("a", 24);
+            collectionToBuild[9] = new EntryPair("o", 14);
             heap.build(collectionToBuild);
 
-            int[] collection= {0,12,4,24,14,18,6,38,30,15};
+            int[] collection = { 0, 12, 4, 24, 14, 18, 6, 38, 30, 15 };
             int[] heapPriorities = new int[10];
             EntryPair[] heapArray = heap.getHeap();
-            for (int i=0;i<heapPriorities.length;i++) {
-                heapPriorities[i] = heapArray[i+1].getPriority();
+            for (int i = 0; i < heapPriorities.length; i++) {
+                heapPriorities[i] = heapArray[i + 1].getPriority();
             }
-            assertArrayEquals("insert created different heap than expected",
-                    collection, heapPriorities);
+            assertArrayEquals("insert created different heap than expected", collection, heapPriorities);
         } catch (Exception e) {
             fail("Exception thrown: " + e.getMessage());
         }
@@ -427,14 +421,8 @@ public class MinBinHeapTests {
     public void buildLecture() {
         Heap_Interface heap = new MinBinHeap();
 
-        EntryPair[] array = {
-            new EntryPair("abc", 6),
-            new EntryPair("bcd", 12),
-            new EntryPair("cde", 9),
-            new EntryPair("def", 4),
-            new EntryPair("efg", 5),
-            new EntryPair("fgh", 3),
-        };
+        EntryPair[] array = { new EntryPair("abc", 6), new EntryPair("bcd", 12), new EntryPair("cde", 9),
+                new EntryPair("def", 4), new EntryPair("efg", 5), new EntryPair("fgh", 3), };
 
         heap.build(array);
 
@@ -448,7 +436,8 @@ public class MinBinHeapTests {
         assertEquals(6, heap.size());
     }
 
-    // Adapted from http://stackoverflow.com/questions/4157159/algorithm-for-checking-if-an-array-with-n-elements-is-a-minimum-heap
+    // Adapted from
+    // http://stackoverflow.com/questions/4157159/algorithm-for-checking-if-an-array-with-n-elements-is-a-minimum-heap
     public static boolean isMinHeap(EntryPair arr[], int rootIndex, int heapSize) {
         boolean isMinH = true;
         int lChild = 2 * rootIndex + 1;
