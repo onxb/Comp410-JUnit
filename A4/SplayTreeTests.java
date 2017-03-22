23fc75ffb5e21531cf9681ae1603ee7cf973ce21
David Ben Knoble <ben.knoble@gmail.com>
2017-03-21 20:44:13 -0400

    A4 test scaffolding

---
+++ A4/SplayTreeTests.java
@@ -0,0 +1,12 @@
+package SPLT_A4;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SplayTreeTests {
+    
+    @Test
+    public void doNothing() {
+        assertTrue("Should pass; just scaffolding for now", true);
+    }
+}
